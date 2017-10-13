3146711ae4538f39e4e3a6fc36b1a7429f856a01
maxdoctorkurt <maxdoctorkurt@mail.ru>
2017-10-13 10:59:20 +0300

    initial

---
+++ vocsearch.py
@@ -0,0 +1,56 @@
+import re
+import pprint
+from beautifultable import BeautifulTable
+
+langs = {
+"ru" : open("ru.xml", "r", encoding="utf-8"),
+"uk" : open("uk.xml", "r", encoding="utf-8"),
+"en" : open("en.xml", "r", encoding="utf-8")
+}
+
+lang_names = {}
+names_set = set()
+commented_names = {}
+
+for lang in langs:
+    temp = []
+    commented = []
+    for line in langs[lang].readlines():
+        result = re.search(r'name="(\w+)"',line)
+        if result != None:
+            name = result.group(1)
+            temp.append(name)
+            names_set.add(name)
+
+            if "<!--" in line:
+                commented.append(name)
+    commented_names[lang] = commented
+
+    lang_names[lang] = temp
+
+hat = ['*']
+
+hat.extend(lang_names.keys())
+
+table = BeautifulTable()
+
+table.append_row(hat)
+
+for name in sorted(names_set):
+
+    row = [name]
+
+    for lang in hat:
+        if lang != '*':
+
+            if name in commented_names[lang]:
+                row.append("C")
+            elif name in lang_names[lang]:
+                row.append("+")
+            else:
+                row.append(" ")
+
+    table.append_row(row)
+
+print(table)
+print(commented_names)
